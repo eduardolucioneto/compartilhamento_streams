e93d57148e8a7ce1845385199cdb3375076ee5b2
unknown <eduardolucioneto@gmail.com>
2025-03-04 12:41:28 -0300

    update

--- setup/settings.py
+++ setup/settings.py
@@ -16,6 +16,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS= ["https://compartilhamentostreams-production.up.railway.app/"]
+
 # usar variáveis de ambiente do banco de dados
 
 DATABASES = {      
